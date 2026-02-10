f0ea1ec47f20f02f01c7c5a609ae753e99298abb
viowb <violet.windebank@gmail.com>
2026-02-10 10:38:32 +0000

    Python_BE 1st Commit

---
+++ Dictionarydatatype.py
@@ -0,0 +1,16 @@
+# a sample dictionary variable
+
+a = {1: "first name", 2: "last name", "age": 33}
+
+# print value having key=1
+print(a[1])
+# print value having key=2
+print(a[2])
+# print value having key="age"
+print(a["age"])
+
+dict = {}
+dict["Firstname"] = "violet"
+dict["lastname"] = "Bank"
+dict["gender"] = "female"
+print(dict)
