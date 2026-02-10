a865c3eb653abb99c3b2bcad4f895b5be26fa255
viowb <violet.windebank@gmail.com>
2026-02-10 10:40:27 +0000

    Python_BE 2nd Commit

---
+++ Tupledatatype.py
@@ -0,0 +1,9 @@
+# tuple having only integer type of data
+a = (1, 2, 3, 4)
+print(a)  # prints the whole tuple
+
+# tuple having multiple type of data
+b = ("hello", 1, 2, 3, "go")
+print(b)  # prints the whole tuple
+
+# index of tuples are also 0 based.
